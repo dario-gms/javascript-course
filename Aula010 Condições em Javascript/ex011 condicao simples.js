6e217ef327396745a3ce78996142798c449d4243
dario-gms <dario.gms@hotmail.com.br>
2021-07-09 12:32:49 -0300

    criado arquivo js
    arquivo js com codigo js

---
+++ Aula010 Condições em Javascript/ex011 condicao simples.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu veículo é ${vel}Km/h`)
+if (vel > 60){ //condicao simples
+    console.log('Você ultrapassou o limite de velocidde permitido, você foi MULTADO!')    
+}
+console.log('Dirija sempre com cuidado!')
