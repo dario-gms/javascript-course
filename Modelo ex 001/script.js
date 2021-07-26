461a6b7f2f2be42beee4c977080f9241e64e12d0
dario-gms <dario.gms@hotmail.com.br>
2021-07-26 16:30:36 -0300

    Incluido algumas pequenas mudanças no código

--- Modelo ex 001/script.js
+++ Modelo ex 001/script.js
@@ -3,15 +3,19 @@ function carregar () {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var minuto = data.getMinutes() 
+    msg.innerHTML = `Agora são <strong>${hora}:${minuto}</strong>.`
     if(hora >= 0 && hora < 12) {
         img.src = 'manhã.png'
         document.body.style.background = '#6b9114'
+        msg.innerHTML += '<p>Tenha um bom dia</p>'
     } else if (hora >= 12 && hora <=18) {
         img.src = 'tarde.png'
         document.body.style.background = '#6693c4'
+        msg.innerHTML += '<p>Tenha uma boa tarde</p>'
     } else {
         img.src = 'noite.png'
         document.body.style.background = '#0e151d'
+        msg.innerHTML += '<p>Tenha uma boa noite</p>'
     }
 }
