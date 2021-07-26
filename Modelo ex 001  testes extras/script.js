c912d46c0411e5567f1414707988e7f19494d578
dario-gms <dario.gms@hotmail.com.br>
2021-07-26 17:38:47 -0300

    site de ponto 2.0

--- Modelo ex 001  testes extras/script.js
+++ Modelo ex 001  testes extras/script.js
@@ -1,15 +1,15 @@
-function carregar () {
+function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    var minuto = data.getMinutes() 
+    var minuto = data.getMinutes()
     msg.innerHTML = `Agora são <strong>${hora}:${minuto}</strong>.`
-    if(hora >= 0 && hora < 12) {
+    if (hora >= 0 && hora < 12) {
         img.src = 'manhã.png'
         document.body.style.background = '#6b9114'
         msg.innerHTML += '<p>Tenha um bom dia</p>'
-    } else if (hora >= 12 && hora <=18) {
+    } else if (hora >= 12 && hora <= 18) {
         img.src = 'tarde.png'
         document.body.style.background = '#6693c4'
         msg.innerHTML += '<p>Tenha uma boa tarde</p>'
@@ -18,4 +18,18 @@ function carregar () {
         document.body.style.background = '#0e151d'
         msg.innerHTML += '<p>Tenha uma boa noite</p>'
     }
+}
+function marcar() {
+    alert("Marcação realizada com sucesso!\nBom trabalho!");
+}
+function encerrar() {
+    var fim;
+    var r = confirm("Deseja encerrar o expediente?");
+    if (r == true) {
+        fim = "Expediente encerrado com sucesso!\nBom descanso!";
+    }
+    else {
+        fim = "Ação cancelada com sucesso!";
+    }
+    document.getElementById("final").innerHTML = fim;
 }
