eda4117734a5aa3ca26e64b86297a7f1b7bab25d
dario-gms <dario.gms@hotmail.com.br>
2021-07-28 13:05:29 -0300

    Realizado algumas pequenas mudanças
    Agora na tela aparece a hora em que foi realizado a marcação de ponto.

--- Ponto 2.0/_javascript/script.js
+++ Ponto 2.0/_javascript/script.js
@@ -8,7 +8,7 @@ function carregar() {
     if (hora >= 0 && hora < 12) {
         img.src = '_imagens/manhã.png'
         document.body.style.background = '#1fa64c'
-        msg.innerHTML += '<p>Tenha um bom dia</p>'
+        msg.innerHTML += '<p>Tenha um bom dia.</p>'
     } else if (hora >= 12 && hora < 18) {
         img.src = '_imagens/tarde.png'
         document.body.style.background = '#6693c4'
@@ -20,13 +20,29 @@ function carregar() {
     }
 }
 function marcar() {
-    alert("Marcação realizada com sucesso!\nBom trabalho!");
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    var ini;
+    var s = confirm("Iniciar expediente?");
+    if (s == true) {
+        ini = `Expediente iniciado às ${hora}:${minuto}. \nBom Trabalho!`;
+    }
+    else {
+        alert("Ação cancelada!")
+        ini = "Expediente não iniciado";
+    }
+    document.getElementById("inicio").innerHTML = ini;
 }
+
 function encerrar() {
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
     var fim;
     var r = confirm("Deseja encerrar o expediente?");
     if (r == true) {
-        fim = "Expediente encerrado com sucesso!\nBom descanso!";
+        fim = `Expediente encerrado às ${hora}:${minuto}. \nBom descanso!`;
     }
     else {
         alert("Ação cancelada!")
