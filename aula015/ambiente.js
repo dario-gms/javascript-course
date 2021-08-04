cc0a497403f9427be9b3ab332edb012e78e6d4b4
dario-gms <dario.gms@hotmail.com.br>
2021-08-04 18:06:28 -0300

    Contador
    Criado exercicio do contador

---
+++ aula015/ambiente.js
@@ -0,0 +1,17 @@
+//ex 1
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+//ex 2
+var d = 1
+do {
+    console.log(d)
+    d++
+} while (d <= 6)
+
+//ex 3
+for (var b = 1; b <= 7; b++) {
+    console.log(b)
+}
