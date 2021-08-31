347c59a5b7d2e21512cc84570c2a909b312169f9
dario-gms <dario.gms@hotmail.com.br>
2021-08-31 17:04:37 -0300

    tabela concluida com sucesso

---
+++ Ponto 2.0/_javascript/historico.js
@@ -0,0 +1,44 @@
+var records = [
+    {
+        local: moment.locale('pt-br'),
+        date: moment().format('D/MM/YYYY'),
+        entrance: moment().format('LTS'),
+        exit: moment().format('LTS'),
+        status: moment().format('dddd')
+    }
+];
+
+function addTable() {
+    let tBody = document.getElementById("minhaTabela");
+
+    for (record in records) {
+        let tr = document.createElement('tr');
+        for (item in record) {
+            var tdData = document.createElement('td');
+            let data = records[record]["date"];
+            tdData.appendChild(document.createTextNode(data));
+            tdData.style.border = '2px solid black';
+            tr.appendChild(tdData);
+
+            var tdEntrada = document.createElement('td');
+            let entrada = records[record]["entrance"];
+            tdEntrada.appendChild(document.createTextNode(entrada));
+            tdEntrada.style.border = '2px solid black';
+            tr.appendChild(tdEntrada);
+
+            var tdSaida = document.createElement('td');
+            let saida = records[record]["exit"];
+            tdSaida.appendChild(document.createTextNode(saida));
+            tdSaida.style.border = '2px solid black';
+            tr.appendChild(tdSaida);
+
+            var tdStatus = document.createElement('td');
+            let status = records[record]["status"];
+            tdStatus.appendChild(document.createTextNode(status));
+            tdStatus.style.border = '2px solid black';
+            tr.appendChild(tdStatus);
+        }
+        tBody.appendChild(tr);
+    }
+}
+addTable();
--- Ponto 2.0/_javascript/main.js
+++ Ponto 2.0/_javascript/main.js
@@ -1,6 +1,7 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    
     var data = new Date()
     var hora = data.getHours()
     var minuto = data.getMinutes()
