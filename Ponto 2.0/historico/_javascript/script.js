fbf68adf2491c121fada3928fdcdbc43970f3723
dario-gms <dario.gms@hotmail.com.br>
2021-08-31 16:12:09 -0300

    tabela de exemplo concluída

--- Ponto 2.0/historico/_javascript/script.js
+++ Ponto 2.0/historico/_javascript/script.js
@@ -1,45 +1,45 @@
-
-let dia = new Date();
 var records = [
+
     {
-        in: dia,
-        out: dia,
-    },
-    {
-        in: dia,
-        out: dia,
-    },
-    {
-        in: dia,
-        out: dia,
-    },
-    {
-        in: dia,
-        out: dia,
-    },
-    {
-        in: dia,
-        out: dia,
+        local: moment.locale('pt-br'),
+        date: moment().format('D/MM/YYYY'),
+        entrance: moment().format('LTS'),
+        exit: moment().format('LTS'),
+        status: moment().format('dddd')
     }
 ];
 
 function addTable() {
-    let tableBody = document.getElementById("minhaTabela"); //renomear para tbody
+    let tBody = document.getElementById("minhaTabela");   
 
     for (record in records) {
         let tr = document.createElement('tr');
         for (item in record) {
+            var tdData = document.createElement('td');
+            let data = records[record]["date"];
+            tdData.appendChild(document.createTextNode(data));
+            tdData.style.border = '2px solid black';
+            tr.appendChild(tdData);
+
             var tdEntrada = document.createElement('td');
-            let entrada = records[record]["in"];
+            let entrada = records[record]["entrance"];
             tdEntrada.appendChild(document.createTextNode(entrada));
+            tdEntrada.style.border = '2px solid black';
             tr.appendChild(tdEntrada);
 
             var tdSaida = document.createElement('td');
-            let saida = records[record]["out"];
+            let saida = records[record]["exit"];
             tdSaida.appendChild(document.createTextNode(saida));
+            tdSaida.style.border = '2px solid black';
             tr.appendChild(tdSaida);
+
+            var tdStatus = document.createElement('td');
+            let status = records[record]["status"];
+            tdStatus.appendChild(document.createTextNode(status));
+            tdStatus.style.border = '2px solid black';
+            tr.appendChild(tdStatus);
         }
-        tableBody.appendChild(tr);
+        tBody.appendChild(tr);
     }
 }
 addTable();
